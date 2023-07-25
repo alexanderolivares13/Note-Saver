57d276733531f7d8297f81ecaf3891406bb52758
alexanderolivares13 <124226984+alexanderolivares13@users.noreply.github.com>
2023-07-24 21:42:56 -0700

    completed the readme file, added images that are displayed on the readme. added a few comments to the server.js file

--- server.js
+++ server.js
@@ -8,12 +8,14 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
+//  app.use is setting up the api endpoint that is routed from the index and the notes.js files in the routes folder
 app.use('/api', api);
 
+// this sets up the endpoint for the notes section and sends a static page of the notes.html file
 app.get('/notes', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
-
+// with the catchall the user will be sent to the index page if the server endpoint does not exist
 app.get('*', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/index.html'))
 );
